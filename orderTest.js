7361810c05e6607add54599bafe5813777f766a7
anastafj197 <frankie.anastasia@gmail.com>
2018-09-06 13:01:17 -0400

    Triangulation
    - Reduce

--- orderTest.js
+++ orderTest.js
@@ -4,9 +4,18 @@ if(orderTotal({
 		{ name: 'Dog cage', price 55 }
 	]	
 }) !== 64) {
-	throw new Error('Check Fail')
+	throw new Error('Check Fail: Example 1')
+}
+
+if(orderTotal({
+	items: [
+		{ name: 'Dog collar', price 13 },
+		{ name: 'Dog toy', price 7 }
+	]	
+}) !== 20) {
+	throw new Error('Check Fail: Example 2')
 }
 
 function orderTotal() {
-	
+	return order.items.reduce((prev, cur) => cur.price + prev, 0)
 }
