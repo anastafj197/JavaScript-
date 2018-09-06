da0af7eebff975e99de7424052af31e5dd058993
anastafj197 <frankie.anastasia@gmail.com>
2018-09-05 23:44:04 -0400

    Shipping
    Wrong comments

--- store.js
+++ store.js
@@ -1,16 +1,26 @@
-# Frank Anastasia 
-# Practice unit testing 
-# Online store order 
+// Frank Anastasia 
+// Practice unit testing 
+// Online store order 
 
 const order = {
 	items: [
 		{ name: 'Bird food',         price: 8,   quantity: 15 },
-		{ name: 'Bird cage (small)', price: 300, quantity: 3 }
+		{ name: 'Bird cage (small)', price: 300, quantity: 3 },
+		{ name: 'Shipping',          price: 40,  shipping: true }
 	]
 }
 
-# 
-const orderTotal = order => order.items
-	.reduce((prev, cur) => prev + cur.price * cur.quantity, 0)
+//
+const orderTotal = order => {
+	const totalItems = order.items 
+		.filter(x => !x.shipping)
+		.reduce((prev, cur) => prev + cur.price * cur.quantity, 0)
+	const shippingItem = 
+		order.items.find(x => !!x.shipping)
+	const shipping = 
+		totalItems > 1000 ? 0 : shippingItem .price 
+	return totalItems + shipping 
+}
 
 result = orderTotal(order)
+console.log(result)
