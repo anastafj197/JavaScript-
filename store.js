f3fa9bce6ebb92c60ee74b41922147337bb724fb
anastafj197 <frankie.anastasia@gmail.com>
2018-09-05 23:35:21 -0400

    Adding complexity

--- store.js
+++ store.js
@@ -4,12 +4,13 @@
 
 const order = {
 	items: [
-		{ name: 'Bird food', price: 8 },
-		{ name: 'Bird cage (small)', price: 300 }
+		{ name: 'Bird food',         price: 8,   quantity: 15 },
+		{ name: 'Bird cage (small)', price: 300, quantity: 3 }
 	]
 }
 
+# 
 const orderTotal = order => order.items
-	.reduce((prev, cur) => prev + cur.price, 0)
+	.reduce((prev, cur) => prev + cur.price * cur.quantity, 0)
 
 result = orderTotal(order)
