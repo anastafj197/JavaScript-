f4ab480ab4bd6680e1370a0bead3eeb48ae3c589
anastafj197 <frankie.anastasia@gmail.com>
2018-09-06 13:34:44 -0400

    Completed tests

--- orderTest.js
+++ orderTest.js
@@ -1,21 +1,44 @@
+// Frank Anastasia 
+// Test first approach based on order.js 
+
+function orderTotal() {
+	return order.items.reduce((prev, cur) => cur.price * (cur.quanitity || 1) + prev, 0)
+}
+
+// Adding quality support 
+if(orderTotal ({
+	items: [
+		{ name: 'Dog bed', price: 17, quanitity: 3 }
+	]
+}) !== 51) {
+	throw new Error('Check Fail: quanitity')
+}
+
+// No quanitity 
+if(orderTotal ({
+	items: [
+		{ name: 'Dog bed', price: 17 }
+	]
+}) !== 17) {
+	throw new Error('Check Fail: No quanitity specified')
+}
+
+// Example 1
 if(orderTotal({
 	items: [
-		{ name: 'Dog food', price 9 },
-		{ name: 'Dog cage', price 55 }
+		{ name: 'Dog food', price: 9, quanitity: 2 },
+		{ name: 'Dog cage', price: 55, quanitity: 1 }
 	]	
-}) !== 64) {
+}) !== 73) {
 	throw new Error('Check Fail: Example 1')
 }
 
+// Example 2
 if(orderTotal({
 	items: [
-		{ name: 'Dog collar', price 13 },
-		{ name: 'Dog toy', price 7 }
+		{ name: 'Dog collar', price: 13, quanitity: 1 },
+		{ name: 'Dog toy', price: 7, quanitity: 1 }
 	]	
 }) !== 20) {
 	throw new Error('Check Fail: Example 2')
-}
-
-function orderTotal() {
-	return order.items.reduce((prev, cur) => cur.price + prev, 0)
 }
