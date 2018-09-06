faa8f327ae17e8113d4785f355f20094ff8a9903
anastafj197 <frankie.anastasia@gmail.com>
2018-09-06 16:49:11 -0400

    Beginning higher order functions

---
+++ functional.js
@@ -0,0 +1,23 @@
+// Frank 	
+// Practice functional programming with JavaScript 
+
+var animals = [
+{ name: 'Fluff',  species: 'rabbit' },
+{ name: 'Polo',   species: 'dog' },
+{ name: 'Nikki',  species: 'dog' },
+{ name: 'Nemo',   species: 'fish' },
+{ name: 'Sneaky', species: 'cat'},
+{ name: 'Fuzzy',  species: 'cat'}
+]
+
+
+// decoupled 
+var isDog = function(animal) {
+	return animal.species === 'dogs'
+}
+
+// filter will loop through each item in the array 
+// and for each item it wilol pass it into the call back function 
+// expect true or false if it should be in the new array
+// returns new filtered array 
+var dogs = animals.filter(isDog)
